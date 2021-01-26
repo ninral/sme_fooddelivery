5720441f79a7ae0104b74d182a7bb079fb31e9a5
Hazeman99 <hazem.xbox@gmail.com>
2021-01-26 22:33:30 +0800

    added login and sign up

---
+++ pages/signin.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <v-container>
+            <v-row class="justify-center">
+                <v-col cols="8">
+                    <v-card class="red pa-6 mb-4">
+                        <v-card-title class="white--text mb-3 signin-header">Sign In</v-card-title>
+
+                        <v-form class="ma-2">
+                            <v-text-field class="white--text mb-3" v-model="email" placeholder="Email" :rules="[rules.required, rules.email]"></v-text-field>
+
+                            <v-text-field class="white--text mb-3" v-model="password" placeholder="Password" type="password" :rules="[rules.required]"></v-text-field>
+
+                            <v-card-actions class="justify-center mb-3">
+                                <v-btn v-on:click.prevent="signin()">Sign In</v-btn>
+                            </v-card-actions>
+                        </v-form>
+                    </v-card>
+                    <v-alert type="error" v-if="error">
+                        {{ error }}
+                    </v-alert>
+                    <v-alert type="success" v-if="success">
+                        {{ success }}
+                    </v-alert>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+    data() {
+        return {
+            email: undefined,
+            password: undefined,
+            error: "",
+            success: "",
+            rules: {
+                required: value => !!value || 'Required',
+                email: value => {
+                    const pattern =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    return pattern.test(value) || 'Invalid e-mail'
+                }
+            }
+        }
+    },
+    methods: {
+        signin(){
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(data => {
+                    this.success = "Successfully signed in"
+                    let user = {
+                        id: data.user.uid,
+                        displayName: data.user.displayName,
+                        email: data.user.email
+                    }
+                    this.$store.dispatch("user/setUser", { user: user, loggedIn: true })
+                        .then(() => {
+                            this.$router.push("/dashboard")
+                        })
+                })
+                .catch(err => {
+                    this.error = err.message
+                })
+        }
+    },
+}
+</script>
